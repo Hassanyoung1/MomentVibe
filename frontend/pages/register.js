4e710de5ca673c9409f7adda6baa53724e3b3cdb
buildveloper <christopherlegacy01@gmail.com>
2025-03-19 21:19:41 +0100

    just started the registration page

---
+++ frontend/pages/register.js
@@ -0,0 +1,6 @@
+import Logo from "../components/Logo";
+import AuthSwitch from "../components/AuthSwitch";
+import InputField from "../components/InputField";
+import SocialAuth from "../components/SocialAuth";
+import { useState } from 'react';
+import { useRouter } from 'next/router';
