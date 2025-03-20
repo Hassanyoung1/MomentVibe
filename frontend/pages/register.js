dfd0bd8c49dcde3e32ed967285665e8b7a909781
buildveloper <christopherlegacy01@gmail.com>
2025-03-20 20:46:03 +0100

    few lines of codes

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -4,3 +4,9 @@ import InputField from "../components/InputField";
 import SocialAuth from "../components/SocialAuth";
 import { useState } from 'react';
 import { useRouter } from 'next/router';
+
+function Register(){
+    const [formData, setFormData] = useState { name: "", email: "", password: "", confirmPassword: "" };
+    const [loading, setLoading] = useState (false);
+    const [error, setError] = useState("");
+}
